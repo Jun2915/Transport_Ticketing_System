783d2159db48b8f616b9af7411059531bc1d045a
Jun <l.jun0310@gmail.com>
2025-11-20 14:08:28 +0800

    .

--- ASS_WMS/Controllers/AccountController.cs
+++ ASS_WMS/Controllers/AccountController.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using ASS_WMS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASS_WMS.Controllers;
+
+public class AccountController(DB db, Helper hp) : Controller
+{
+    // GET: Account/Register
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    // POST: Account/Register
+    [HttpPost]
+    public IActionResult Register(RegisterVM vm)
+    {
+        if(ModelState.IsValid("Email") && db.Users.Any(u => u.Email == vm.Email))
+        {
+            ModelState.AddModelError("Email", "Duplicated Email.");
+        }
+
+        if (ModelState.IsValid("Photo"))
+        {
+            var err = hp.ValidatePhoto(vm.Photo);
+            if (err != "") ModelState.AddModelError("Photo", err);
+        }
+
+        if (ModelState.IsValid)
+        {
+            // Insert member
+            db.Members.Add(new()
+            {
+                Email = vm.Email,
+                //Hash = hp.HashPassword(vm.Password),
+                Name = vm.Name,
+                PhotoURL = hp.SavePhoto(vm.Photo, "photos"),
+            });
+            db.SaveChanges();
+
+            TempData["Info"] = "Register successfully. Please login.";
+            return RedirectToAction("Login");
+        }
+
+        return View(vm);
+    }
+}
